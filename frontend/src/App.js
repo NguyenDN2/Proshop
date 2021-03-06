201bd8fd09669ccdc430f9ab2854f8c6741a5ca3
nguyedn2 <nguyendn2@fpt.com.vn>
2021-03-06 11:01:20 +0700

    up component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+import React from "react";
+import Header from "./components/Headers";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <Router>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
 export default App;
